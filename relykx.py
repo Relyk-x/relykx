651357046ca564d4a927dcbedbbac17b3e6f076e
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-18 12:03:28 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -53,13 +53,13 @@ async def kick(ctx, user: discord.Member):
 	await bot.kick(user)
 
 @bot.command(pass_context=True)
-async def clear(ctx, user: discord.Member, msglimit : int):
-	if user.id == '257784039795064833':
+async def clear(ctx, msglimit : int):
+	if msglimit : int < 100
 		deleted = await bot.purge_from(ctx.message.channel, limit=msglimit)
 		embed = discord.Embed(description='Purged **{}** message(s) from this channel! ⚠'.format(len(deleted)), color=0xffafc9,)
 		await bot.say(embed=embed)
 	else:
-		embed = discord.Embed(description="You don't have the permissions to use this command", color=0xffafc9,)
+		embed = discord.Embed(description="Sorry that's too much...", color=0xffafc9,)
 		await bot.say(embed=embed)
 	
 @bot.command(pass_context=True)
