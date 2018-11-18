6154b878309302c4e1cf535f7cb26e3850219034
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-18 12:06:29 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -55,12 +55,12 @@ async def kick(ctx, user: discord.Member):
 @bot.command(pass_context=True)
 async def clear(ctx, msglimit : int):
 	if msglimit : int < 100
-	    deleted = await bot.purge_from(ctx.message.channel, limit=msglimit)
-	    embed = discord.Embed(description='Purged **{}** message(s) from this channel! ⚠'.format(len(deleted)), color=0xffafc9,)
-	    await bot.say(embed=embed)
+	deleted = await bot.purge_from(ctx.message.channel, limit=msglimit)
+	embed = discord.Embed(description='Purged **{}** message(s) from this channel! ⚠'.format(len(deleted)), color=0xffafc9,)
+	await bot.say(embed=embed)
 	else:
-	    embed = discord.Embed(description="Sorry that's too much...", color=0xffafc9,)
-	    await bot.say(embed=embed)
+	embed = discord.Embed(description="Sorry that's too much...", color=0xffafc9,)
+	await bot.say(embed=embed)
 	
 @bot.command(pass_context=True)
 async def about(ctx):
