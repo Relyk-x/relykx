2521ce35a8088e482f05f3dd4df46971d52fe0dd
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-19 21:51:22 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -51,9 +51,9 @@ async def serverinfo(ctx):
 	embed.set_thumbnail(url=ctx.message.server.icon_url)
 	embed.add_field(name="Name:", value=ctx.message.server.name, inline=True)
 	embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
-	embed.add_field(name="Created", value=ctx.message.server.created_at, inline=True)
 	embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
 	embed.add_field(name="Members", value=len(ctx.message.server.members), inline=True)
+	embed.add_field(name="Created", value=ctx.message.server.created_at, inline=True)
 	await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
