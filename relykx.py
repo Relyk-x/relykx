6353d8b537d96feab6e0cedd0d835c89f35df484
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-19 18:37:39 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -132,6 +132,31 @@ async def time(ctx):
 ##############################################################################################################################
 	
 @bot.command(pass_context=True)
+async def hi(ctx):
+	randomlist = ['It is certain.',
+                      'It is decidedly so.',
+                      'Without a doubt.',
+                      'Yes - definitely.',
+                      'You may rely on it.',
+                      'As I see it, yes.',
+                      'Most likely.',
+                      'Outlook good.',
+                      'Yes.',
+                      'Signs point to yes.',
+                      'Reply hazy, try again',
+                      'Ask again later.',
+                      'Better not tell you now.',
+                      'Cannot predict now.',
+                      'Concentrate and ask again.',
+                      "Don't count on it.",
+                      'My reply is no.',
+                      'My sources say no.',
+                      'Outlook not so good.',
+                      'Very doubtful.',
+                     ]
+	await bot.say("%s" %(random.choice(randomlist),))
+	
+@bot.command(pass_context=True)
 async def wallpaper(ctx):
 	embed = discord.Embed(color=0xffafc9,)
 	embed.set_image(url='https://picsum.photos/1280/720/?image=' + str(random.randint(1, 999)))
