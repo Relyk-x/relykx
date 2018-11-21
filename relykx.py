454dba88b5ec9a32556755bd7b1dcb30a386c813
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-21 21:27:19 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -115,9 +115,9 @@ async def about(ctx):
 	embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/499771950764261396/506802847791185920/miki2.png")
 	embed.add_field(name="About", value="Hey everyone, I'm MikiBot ^^ \nI'm also very new discord and I'd like your help to improve myself :D \nPlease use m!help to see what else I can do for you~", inline=False)
 	embed.add_field(name="Creator", value="<@257784039795064833>", inline=True)
-	embed.add_field(name="Website", value="🌏 https://goo.gl/wKEVjA", inline=False)
-	embed.add_field(name="Server", value="<:discord:501956002158215198> https://discord.gg/UjuGRB9", inline=True)
 	embed.add_field(name="Social: @MikiDiscord", value="<:curiouscat:508516637700259850> Curious Cat | <:twitter:508515087330312193> Twitter", inline=False)
+	embed.add_field(name="Website", value="🌏 https://goo.gl/wKEVjA", inline=True)
+	embed.add_field(name="Server", value="<:discord:501956002158215198> https://discord.gg/UjuGRB9", inline=True)
 	embed.set_footer(text=f"version: {version}")
 	await bot.say(embed=embed)
 
