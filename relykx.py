06adaf3272dfdd7fdd67021c8418c3f796ee4ec5
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-21 13:54:25 +0800

    Update relykx.py

--- relykx.py
+++ relykx.py
@@ -54,7 +54,7 @@ async def serverinfo(ctx):
 	embed = discord.Embed(title="📋 {}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0xffafc9)
 	embed.set_thumbnail(url=ctx.message.server.icon_url)
 	embed.add_field(name="ID:", value=ctx.message.server.id, inline=True)
-	embed.add_field(name="Owner:", value="<@257784039795064833>", inline=True)
+	embed.add_field(name="Owner:", value=ctx.mssage.server.owner.mention, inline=True)
 	embed.add_field(name="Region:", value=ctx.message.server.region, inline=False)
 # embed.add_field(name="Varification level:, value=?, inline=True)
 	embed.add_field(name="Roles:", value=len(ctx.message.server.roles), inline=True)
